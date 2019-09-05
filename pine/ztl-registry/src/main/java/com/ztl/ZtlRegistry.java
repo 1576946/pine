6fa02bb4fca57dc7aee2cfc33679ea9b21c18cc4
赵天龙 <zhaotianlong@i-billing.cn>
2019-09-05 21:12:26 +0800

    配置eureka依赖，并配置服务端口 配置服务名和访问端口

---
+++ pine/ztl-registry/src/main/java/com/ztl/ZtlRegistry.java
@@ -0,0 +1,13 @@
+package com.ztl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ZtlRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(ZtlRegistry.class);
+    }
+}
