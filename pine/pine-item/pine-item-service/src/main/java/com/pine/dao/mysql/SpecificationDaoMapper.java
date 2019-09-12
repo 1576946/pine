75ee6f18a04a0285a82ce2d4fc0f7f3692892439
赵天龙 <zhaotianlong@i-billing.cn>
2019-09-12 14:43:20 +0800

    查询商品分类的 dao层

--- pine/pine-item/pine-item-service/src/main/java/com/pine/dao/mysql/SpecificationDaoMapper.java
+++ pine/pine-item/pine-item-service/src/main/java/com/pine/dao/mysql/SpecificationDaoMapper.java
@@ -1,4 +1,11 @@
 package com.pine.dao.mysql;
 
+import com.pine.bean.dto.SpecificationDO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface SpecificationDaoMapper {
+    @Select("selsct * from tb_specification where Category_id = #{id}")
+    SpecificationDO querySpecificationByCategoryId(Long id);
 }
