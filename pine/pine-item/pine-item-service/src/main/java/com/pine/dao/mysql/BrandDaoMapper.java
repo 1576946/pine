def5be2a1351f31a38e2fd191607a90ccf3f0aae
赵天龙 <zhaotianlong@i-billing.cn>
2019-09-12 12:59:33 +0800

    新增商品的dao实现 外加sql语句

--- pine/pine-item/pine-item-service/src/main/java/com/pine/dao/mysql/BrandDaoMapper.java
+++ pine/pine-item/pine-item-service/src/main/java/com/pine/dao/mysql/BrandDaoMapper.java
@@ -2,7 +2,18 @@ package com.pine.dao.mysql;
 
 import com.github.pagehelper.Page;
 import com.pine.bean.dto.BrandDO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 public interface BrandDaoMapper {
     Page<BrandDO> queryBrandByPageAndSort(Integer page, Integer rows, String sortBy, Boolean desc, String key);
+
+    @Insert("INSERT INTO tb_category_brand (category_id,brand_id) VALUES (#{cid},#{bid})")
+    int insertCategoryBrand(Long cid, Long bid);
+
+    @Insert("INSERT INTO tb_brand (name,image,letter) VALUES (#{name},#{image},#{letter})")
+    void saveBrand(BrandDO brand);
+
+    @Select("Select id from tb_brand where name = #{name}")
+    Long findIdByName(String name);
 }
