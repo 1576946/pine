df46c2705d5b9588fb9c163d6192420a8867c9cb
赵天龙 <zhaotianlong@i-billing.cn>
2019-09-12 12:58:46 +0800

    文件上传的service接口

--- pine/pine-item/pine-item-service/src/main/java/com/pine/service/BrandService.java
+++ pine/pine-item/pine-item-service/src/main/java/com/pine/service/BrandService.java
@@ -3,6 +3,10 @@ package com.pine.service;
 import com.pine.bean.dto.BrandDO;
 import com.pine.common.bean.bo.PageResultBean;
 
+import java.util.List;
+
 public interface BrandService {
     PageResultBean<BrandDO> queryBrandByPageAndSort(Integer page, Integer rows, String sortBy, Boolean desc, String key);
+
+    void saveBrand(BrandDO brand, List<Long> cids);
 }
